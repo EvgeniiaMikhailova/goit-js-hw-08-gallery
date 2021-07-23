3778252bfb719c59d82b1b892c0ae29021ea1e15
EvgeniiaMikhailova <64032773+EvgeniiaMikhailova@users.noreply.github.com>
2021-07-24 01:27:53 +0300

    добавляет галерею и модалку

--- app.js
+++ app.js
@@ -63,3 +63,68 @@ const galleryItems = [
         description: 'Lighthouse Coast Sea',
     },
 ];
+
+const galleryContainer = document.querySelector('.js-gallery');
+const galleryMarkup = createImagesMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createImagesMarkup(gallery) {
+    return gallery.map(({ preview, original, description }) => {
+        return `
+    <li class="gallery__item">
+  <a class="gallery__link"
+    href="${original}">
+    <img class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}" />
+  </a>
+</li>`
+    })
+        .join('');
+};
+// console.log(createImagesMarkup(galleryItems));
+
+const gallery = document.querySelector('.js-gallery');
+const galleryItem = document.querySelector('.gallery__item');
+const modalRef = document.querySelector('.lightbox');
+const closeModalBtn = document.querySelector('[data-action="close-lightbox"]');
+const modalImgRef = document.querySelector('.lightbox__image');
+const modalOverlay = document.querySelector('.lightbox__overlay');
+const modalContent = document.querySelector('.lightbox__content');
+
+gallery.addEventListener('click', openModal);
+
+function openModal(e) {
+    e.preventDefault();
+    window.addEventListener('keyup', closeModalESC);
+    if (e.target.localName === 'img') {
+        modalRef.classList.add('is-open');
+        modalImgRef.src = e.target.dataset.source;
+        modalImgRef.alt = e.target.alt;
+    }
+}
+
+closeModalBtn.addEventListener('click', closeModal);
+
+function closeModal(e) {
+    window.removeEventListener('keyup', closeModalESC);
+    modalRef.classList.remove('is-open');
+    modalImgRef.src.innerHTML = '';
+}
+
+function closeModalESC(e) {
+    if (e.key !== 'Escape') {
+        return
+    }
+    closeModal();
+};
+
+modalOverlay.addEventListener('click', onOverlayClick)
+
+function onOverlayClick(e) {
+    if (e.currentTarget === e.target) {
+        closeModal()
+    }
+}
